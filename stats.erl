69b0ceda8f9f56ad6604d5da309a5b3f5ca9edab
Victor Tyutyunov <itdevchief@gmail.com>
2011-11-06 19:09:55 +0200

    flight parser

--- stats.erl
+++ stats.erl
@@ -12,12 +12,65 @@
 %% ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 -record(airport, {iata, name, address, city, state, country, time, date, zone}).
--record(flight, {flight, date, status}).
+-record(flight, {flight, name, date, status, origin, departure, destination, arrival}).
+-record(schedule, {scheduled, estimated, actual, terminal}).
 
 %% ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-parse_flight_attr(Flight, _Tree) ->
-	Flight.
+parse_flight_value(Schedule, []) -> Schedule;
+
+parse_flight_value(Schedule, [<<"Scheduled:">> | Tail]) ->
+	[Value | Tail2] = Tail, 
+	{match, [HH, MM]} = re:run(Value, "([0-9][0-9]):([0-9][0-9])", [{capture, [1,2], list}]),
+	parse_flight_value(Schedule#schedule{scheduled = {list_to_integer(HH), list_to_integer(MM), 0}}, Tail2);
+	
+parse_flight_value(Schedule, [<<"Estimated:">> | Tail]) ->
+	[Value | Tail2] = Tail, 
+	{match, [HH, MM]} = re:run(Value, "([0-9][0-9]):([0-9][0-9])", [{capture, [1,2], list}]),
+	parse_flight_value(Schedule#schedule{estimated = {list_to_integer(HH), list_to_integer(MM), 0}}, Tail2);
+
+parse_flight_value(Schedule, [<<"Actual:">> | Tail]) ->
+	[Value | Tail2] = Tail, 
+	{match, [HH, MM]} = re:run(Value, "([0-9][0-9]):([0-9][0-9])", [{capture, [1,2], list}]),
+	parse_flight_value(Schedule#schedule{actual = {list_to_integer(HH), list_to_integer(MM), 0}}, Tail2);
+
+parse_flight_value(Schedule, [<<"Gate:">> | Tail]) ->
+	[Value | Tail2] = Tail, 
+	parse_flight_value(Schedule#schedule{terminal = Value}, Tail2);
+
+parse_flight_value(Schedule, [<<"Terminal:">> | Tail]) ->
+	[Value | Tail2] = Tail, 
+	parse_flight_value(Schedule#schedule{terminal = Value}, Tail2);
+
+parse_flight_value(Schedule, [_Head | Tail]) ->
+	parse_flight_value(Schedule, Tail).
+
+%% ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+parse_flight_attribute(Flight, [], _) -> Flight;
+
+parse_flight_attribute(Flight, [<<"Departure">>], Tree) ->
+	[Origin] = mochiweb_xpath:execute("//div/a/text()",Tree),
+	[DepartureList] = mochiweb_xpath:execute("//div/text()",Tree),
+	Departure = lists:filter(fun(X) -> X =/= <<>> end, re:split(re:replace(DepartureList, "/[\r\n ]*", <<"/">>, [global, {return, list}]), "\r\n *\t*", [{return, binary}])),
+	Flight#flight{departure = parse_flight_value(#schedule{}, Departure), origin = airport(binary_to_list(Origin))};
+
+parse_flight_attribute(Flight, [<<"Arrival">>], Tree) ->
+	[Destination] = mochiweb_xpath:execute("//div/a/text()",Tree),
+	[ArrivalList] = mochiweb_xpath:execute("//div/text()",Tree),
+	Arrival = lists:filter(fun(X) -> X =/= <<>> end, re:split(re:replace(ArrivalList, "/[\r\n ]*", <<"/">>, [global, {return, list}]),  "\r\n *\t*", [{return, binary}])),
+	Flight#flight{arrival = parse_flight_value(#schedule{}, Arrival), destination = airport(binary_to_list(Destination))};
+
+parse_flight_attribute(Flight, [Text], _) ->
+	{match, [Name]} = re:run(Text, "\\(\\) (.+)\r\n", [{capture, [1], binary}]),
+	Flight#flight{name = Name}.
+	
+%% ---------------------------------------------------------------------------------------------------------------------------------------------------
+	
+parse_flight_component(Flight, []) -> Flight;
+
+parse_flight_component(Flight, [Head|Tail]) -> 
+	parse_flight_component(parse_flight_attribute(Flight, mochiweb_xpath:execute("//div/h2/text()",Head), Head), Tail).
 
 %% ---------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -29,7 +82,7 @@ parse_flight(Flight, Tree) ->
 			{match, [Status]} = re:run(StatusString, "\r\n *([A-Za-z]+)", [{capture, [1], binary}]),
 			[Airline] = mochiweb_xpath:execute("//div[@class='content']/div[@class='uiComponent'][1]/h2/a/text()",Tree),
 			{_Airline, Number} = Flight#flight.flight,
-			parse_flight_attr(Flight#flight{flight = {Airline, Number}, status = Status}, mochiweb_xpath:execute("//div[@class='content']/div[@class='uiComponent']",Tree))
+			parse_flight_component(Flight#flight{flight = {Airline, Number}, status = Status}, mochiweb_xpath:execute("//div[@class='content']/div[@class='uiComponent']",Tree))
 	end.
 
 %% ----------------------------------------------------------------------------------------------
@@ -37,7 +90,7 @@ parse_flight(Flight, Tree) ->
 search_segments(_Flight, []) -> ok;
 
 search_segments(Flight, [Segment|Tail]) ->
-	case re:run(binary_to_list(Segment), "\\?id=([0-9]+)$", [{capture, [1], list}]) of
+	case re:run(Segment, "\\?id=([0-9]+)$", [{capture, [1], list}]) of
 		{match, [Id]} -> search_flight(Flight, lists:concat([?Flight, "id=", Id]));
 		nomatch -> ok
 	end,
@@ -50,7 +103,7 @@ search_flight(Flight, Search) ->
 	Tree = mochiweb_html:parse(re:replace(HTML, "(\\:\\&nbsp\\;)*", "", [{return, list}, global])),
 	Segments = mochiweb_xpath:execute("//div[@class='listItem']/a[@href]/@href",Tree),
 	case length(Segments) of
-		0 -> io:format("Result = ~p~n", [parse_flight(Flight, Tree)]);
+		0 -> io:format("~p~n", [parse_flight(Flight, Tree)]);
 		_ -> search_segments(Flight, Segments)
 	end.
 
@@ -69,7 +122,7 @@ flight(Flight, Date) ->
 parse_airport(Airport, []) -> Airport;
 
 parse_airport(Airport, [<<"Name">>, Value | Tail]) ->
-	Name = re:replace(Value, "(\\(" ++ binary_to_list(Airport#airport.iata) ++ "\\) )", "", [{return, binary}]),
+	Name = re:replace(Value, lists:concat(["(\\(", binary_to_list(Airport#airport.iata), "\\) )"]), "", [{return, binary}]),
 	parse_airport(Airport#airport{name=Name}, Tail);
 
 parse_airport(Airport, [<<"Address">>, Value | Tail]) ->
@@ -84,16 +137,16 @@ parse_airport(Airport, [<<"City">>, Value | Tail]) ->
 	parse_airport(Airport#airport{city = City, state = State, country = Country}, Tail);
 	
 parse_airport(Airport, [<<"Current Time">>, Value | Tail]) ->
-	{match,[HH,MM,SS]} = re:run(binary_to_list(Value), "\\(([0-9][0-9]):([0-9][0-9]):([0-9][0-9])\\)", [{capture, [1,2,3], list}]),
+	{match,[HH,MM,SS]} = re:run(Value, "\\(([0-9][0-9]):([0-9][0-9]):([0-9][0-9])\\)", [{capture, [1,2,3], list}]),
 	parse_airport(Airport#airport{time={list_to_integer(HH), list_to_integer(MM), list_to_integer(SS)}}, Tail);
 
 parse_airport(Airport, [<<"Current Date">>, Value | Tail]) ->
-	[Day, MonthString, Year] = re:split(binary_to_list(Value), "-",[{return, list}]),
+	[Day, MonthString, Year] = re:split(Value, "-",[{return, list}]),
 	{MonthString, Month} = lists:keyfind(MonthString, 1, ?Months),
 	parse_airport(Airport#airport{date={list_to_integer(Year), Month, list_to_integer(Day)}}, Tail);
 
 parse_airport(Airport, [<<"Time Zone">>, Value | Tail]) ->
-	{match, [HH, MM]} = re:run(binary_to_list(Value), "([-|+][0-9]+):([0-9]+)", [{capture, [1,2], list}]),
+	{match, [HH, MM]} = re:run(Value, "([-|+][0-9]+):([0-9]+)", [{capture, [1,2], list}]),
 	parse_airport(Airport#airport{zone = {list_to_integer(HH), list_to_integer(MM), 0}}, Tail);
 
 parse_airport(Airport, [_Attribute, _Value | Tail]) ->
