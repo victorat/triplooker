ea3ec6dca4fa1a71ea5594d184b2b084d60cd0ee
Victor Tyutyunov <itdevchief@gmail.com>
2011-11-08 17:54:18 +0200

    radar online

--- radar.erl
+++ radar.erl
@@ -1,18 +1,23 @@
 -module(radar).
--export([list/1]).
+-export([list/0]).
 
 -define(JSSeconds, 62167219200).
--define(Search, "'(\\w*)':\\['(\\w*)',[0-9\\-\\.]*,[0-9\\-\\.]*,'\\d*','([\\-\\d]*)','\\d*','\\d*','\\w*','\\w*','[\\w\\-\\+]*',([0-9]*)\\]").
+-define(DataExtract, "'([A-Z]+[0-9]+)':\\['([0-9A-F]+)',[0-9\\-\\.]*,[0-9\\-\\.]*,'\\d*','([\\-\\d]*)','\\d*','\\d*','\\w*','\\w*','[\\w\\-\\+]*',([0-9]*)\\]").
+-define(Search, "http://www.flightradar24.com/PlaneFeed.json").
+
 %% -define(Search, "'(\\w*)':\\[").
 
 process_flight([]) -> ok;
 
-process_flight([[Flight, Hex, _, _] | Tail]) ->
-	io:format("Flight = ~p, Hex = ~p~n", [Flight, Hex]),
+process_flight([[Flight, Hex, Altitude, Seconds] | Tail]) ->
+	{Date, {HH,MM,SS}} = calendar:gregorian_seconds_to_datetime(list_to_integer(binary_to_list(Seconds)) + ?JSSeconds),
+	io:format("Flight = ~p, Hex = ~p, Altitude = ~p, DateTime = ~p:~p:~p~n", [Flight, Hex, Altitude, HH, MM, SS]),
 	process_flight(Tail).
 
-list(File) -> 
-	{ok, Binary} = file:read_file(File),
-	% {match, Flights} = re:run(Binary, "\\x22([A-Z]+[0-9]+)\\x22:\\[\\x22([0-9A-F]+)\\x22", [global, {capture, [1,2], binary}]),
-	{match, Flights} = re:run(re:replace(Binary, "\\x22", "'", [global, {return, binary}]), ?Search, [global, {capture, [1,2,3,4], binary}]),
+list() ->
+	inets:start(),
+	{ok, {_Status, _Header, HTML}} = httpc:request(?Search),
+    % {ok, Binary} = file:read_file(File),
+ 	% {match, Flights} = re:run(Binary, "\\x22([A-Z]+[0-9]+)\\x22:\\[\\x22([0-9A-F]+)\\x22", [global, {capture, [1,2], binary}]),
+	{match, Flights} = re:run(re:replace(HTML, "\\x22", "'", [global, {return, binary}]), ?DataExtract, [global, {capture, [1,2,3,4], binary}]),
 	process_flight(Flights).
